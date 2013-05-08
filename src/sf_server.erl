61de260624d3849dc27eed431513c4ba420c7163
V. Glenn Tarcea <glenn.tarcea@gmail.com>
2013-05-08 09:05:59 -0400

    Clean up comments. Move file:close() to terminate and handle case when file is not open.

--- src/sf_server.erl
+++ src/sf_server.erl
@@ -45,13 +45,6 @@ start_link(LSocket) ->
 %%--------------------------------------------------------------------
 %% @private
 %% @doc
-%% Initializes the server
-%%
-%% @spec init(Args) -> {ok, State} |
-%%                     {ok, State, Timeout} |
-%%                     ignore |
-%%                     {stop, Reason}
-%% @end
 %%--------------------------------------------------------------------
 init([LSocket]) ->
     {ok, #state{lsocket = LSocket, fd = not_set}, 0}.
@@ -59,16 +52,6 @@ init([LSocket]) ->
 %%--------------------------------------------------------------------
 %% @private
 %% @doc
-%% Handling call messages
-%%
-%% @spec handle_call(Request, From, State) ->
-%%                                   {reply, Reply, State} |
-%%                                   {reply, Reply, State, Timeout} |
-%%                                   {noreply, State} |
-%%                                   {noreply, State, Timeout} |
-%%                                   {stop, Reason, Reply, State} |
-%%                                   {stop, Reason, State}
-%% @end
 %%--------------------------------------------------------------------
 handle_call(_Request, _From, State) ->
     Reply = ok,
@@ -77,12 +60,6 @@ handle_call(_Request, _From, State) ->
 %%--------------------------------------------------------------------
 %% @private
 %% @doc
-%% Handling cast messages
-%%
-%% @spec handle_cast(Msg, State) -> {noreply, State} |
-%%                                  {noreply, State, Timeout} |
-%%                                  {stop, Reason, State}
-%% @end
 %%--------------------------------------------------------------------
 handle_cast(_Msg, State) ->
     {noreply, State}.
@@ -90,12 +67,6 @@ handle_cast(_Msg, State) ->
 %%--------------------------------------------------------------------
 %% @private
 %% @doc
-%% Handling all non call/cast messages
-%%
-%% @spec handle_info(Info, State) -> {noreply, State} |
-%%                                   {noreply, State, Timeout} |
-%%                                   {stop, Reason, State}
-%% @end
 %%--------------------------------------------------------------------
 %handle_info({tcp, Socket, Filename}, State) when State#state.fd =:= not_set ->
 handle_info({tcp, Socket, Request}, State) when State#state.fd =:= not_set ->
@@ -125,8 +96,7 @@ handle_info(timeout, #state{lsocket = LSocket} = State) ->
     {ok, _Socket} = gen_tcp:accept(LSocket),
     sf_sup:start_child(),
     {noreply, State};
-handle_info({tcp_closed, _Socket}, #state{fd = Fd} = State) ->
-    file:close(Fd),
+handle_info({tcp_closed, _Socket}, State) ->
     {stop, normal, State};
 handle_info(_Info, State) ->
     {noreply, State}.
@@ -134,24 +104,16 @@ handle_info(_Info, State) ->
 %%--------------------------------------------------------------------
 %% @private
 %% @doc
-%% This function is called by a gen_server when it is about to
-%% terminate. It should be the opposite of Module:init/1 and do any
-%% necessary cleaning up. When it returns, the gen_server terminates
-%% with Reason. The return value is ignored.
-%%
-%% @spec terminate(Reason, State) -> void()
-%% @end
 %%--------------------------------------------------------------------
-terminate(_Reason, _State) ->
+terminate(_Reason, #state{fd = Fd}) when Fd =:= not_set ->
+    ok;
+terminate (_Reason, #state{fd = Fd})
+    file:close(Fd),
     ok.
 
 %%--------------------------------------------------------------------
 %% @private
 %% @doc
-%% Convert process state when code is changed
-%%
-%% @spec code_change(OldVsn, State, Extra) -> {ok, NewState}
-%% @end
 %%--------------------------------------------------------------------
 code_change(_OldVsn, State, _Extra) ->
     {ok, State}.
